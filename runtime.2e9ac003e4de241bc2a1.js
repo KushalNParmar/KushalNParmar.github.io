d95e0f55b35f077af2e6c4ed6ac7c3c75be7c539
Kushal Parmar <kushalparmar@fynd.com>
2024-12-09 13:53:35 +0530

    watch

--- runtime.2e9ac003e4de241bc2a1.js
+++ runtime.2e9ac003e4de241bc2a1.js
@@ -115,7 +115,7 @@
 /******/ 		// This function allow to reference async chunks
 /******/ 		__webpack_require__.u = (chunkId) => {
 /******/ 			// return url for filenames based on template
-/******/ 			return "" + chunkId + "." + "d2de42b199d5ebd6b2f0" + ".js";
+/******/ 			return "" + chunkId + "." + "5ef9a4216a3a2d3aab95" + ".js";
 /******/ 		};
 /******/ 	})();
 /******/ 	
